c5f010d3cf1e4ceab62586dcf08416495da9a0bb
Stefan Zweifel <hello@stefanzweifel.io>
2019-06-10 15:32:31 +0200

    Use ENV Variable for Commit Message

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ git config --global user.name "Github Actions"
 
 git add -A
 git status
-git commit -m "$*" --author="$COMMIT_AUTHOR_NAME <$COMMIT_AUTHOR_EMAIL>" || echo "No changes found. Nothing to commit."
+git commit -m "$COMMIT_MESSAGE" --author="$COMMIT_AUTHOR_NAME <$COMMIT_AUTHOR_EMAIL>" || echo "No changes found. Nothing to commit."
 git push -u origin HEAD
