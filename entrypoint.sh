d43700cf6a132ebf1b6e02f2646ca0d3b25d5875
Stefan Zweifel <hello@stefanzweifel.io>
2019-06-10 14:30:50 +0200

    Add Core Logic

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+
+git config --global user.email "actions@github.com"
+git config --global user.name "Github Actions"
+
+git add -A
+git status
+git commit -m "$*" --author="$COMMIT_AUTHOR_NAME <$COMMIT_AUTHOR_EMAIL>" || echo "No changes found. Nothing to commit."
+git push -u origin HEAD
