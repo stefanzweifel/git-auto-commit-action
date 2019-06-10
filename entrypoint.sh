481a78354bc4bf86baa10e98bec0b1953a996b40
Stefan Zweifel <hello@stefanzweifel.io>
2019-06-10 15:32:33 +0200

    Nitpick

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -eu
 
 git config --global user.email "actions@github.com"
-git config --global user.name "Github Actions"
+git config --global user.name "GitHub Actions"
 
 git add -A
 git status
