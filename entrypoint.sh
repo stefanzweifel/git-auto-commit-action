0364ecfe57919e82e6e57cb9d28297caa71e5fbb
Stefan Zweifel <hello@stefanzweifel.io>
2019-08-31 18:34:01 +0200

    Update Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
 set -eu
 
-# Switch to branch from current Workflow run
-# git checkout "${GITHUB_REF:11}"
-
-# Set origin URL
-# git remote set-url origin https://$TOKEN:x-oauth-basic@github.com/$GITHUB_REPOSITORY
-
-git config --global user.email "actions@github.com"
-git config --global user.name "GitHub Actions"
-
-# git add -A
-# git status
-# git commit -m "$INPUT_COMMIT_MESSAGE" --author="$INPUT_COMMIT_AUTHOR_NAME <$INPUT_COMMIT_AUTHOR_EMAIL>" || echo "No changes found. Nothing to commit."
-# git push -u origin HEAD
-
-
-
 # Set up .netrc file with GitHub credentials
 git_setup ( ) {
   cat <<- EOF > $HOME/.netrc
@@ -28,14 +12,10 @@ git_setup ( ) {
         login $GITHUB_ACTOR
         password $GITHUB_TOKEN
 EOF
-  chmod 600 $HOME/.netrc
+    chmod 600 $HOME/.netrc
 
-  # Git requires our "name" and email address -- use GitHub handle
-  # git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
-  # git config user.name "$GITHUB_ACTOR"
-
-  # Push to the current branch if PUSH_BRANCH hasn't been overriden
-  # : ${PUSH_BRANCH:=`echo "$GITHUB_REF" | awk -F / '{ print $3 }' `}
+    git config --global user.email "actions@github.com"
+    git config --global user.name "GitHub Actions"
 }
 
 
@@ -45,12 +25,14 @@ if ! git diff --quiet
 then
     git_setup
 
+    # Switch to branch from current Workflow run
     git checkout "${GITHUB_REF:11}"
-    # git checkout $PUSH_BRANCH
+
     git add .
-    git commit -m "$INPUT_COMMIT_MESSAGE" --author="$INPUT_COMMIT_AUTHOR_NAME <$INPUT_COMMIT_AUTHOR_EMAIL>" || echo "No changes found. Nothing to commit."
+
+    git commit -m "$INPUT_COMMIT_MESSAGE" --author="$INPUT_COMMIT_AUTHOR_NAME <$INPUT_COMMIT_AUTHOR_EMAIL>"
+
     git push --set-upstream origin "${GITHUB_REF:11}"
-    # git push --set-upstream origin $PUSH_BRANCH
 else
     echo "Working tree clean. Nothing to commit."
 fi
