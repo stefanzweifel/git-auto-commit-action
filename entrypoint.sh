8cc484cbc554155d09a09f493ad7f6c19ec8b248
Stefan Zweifel <hello@stefanzweifel.io>
2019-09-20 10:30:36 +0200

    Add ref argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,13 +30,13 @@ then
     echo "Push Branch Value: $PUSH_BRANCH";
 
     # Switch to branch from current Workflow run
-    git checkout -b $PUSH_BRANCH
+    git checkout -b $INPUT_REF
 
     git add .
 
     git commit -m "$INPUT_COMMIT_MESSAGE" --author="$INPUT_COMMIT_AUTHOR_NAME <$INPUT_COMMIT_AUTHOR_EMAIL>"
 
-    git push --set-upstream origin $PUSH_BRANCH
+    git push --set-upstream origin $INPUT_REF
 else
     echo "Working tree clean. Nothing to commit."
 fi
