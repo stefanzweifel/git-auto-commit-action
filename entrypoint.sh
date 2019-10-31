b6dcf9430301384c5a3c21ecab67567cc18307e1
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-10-31 11:18:48 +0100

    Add is_defined and get_value to improve on code's reusability

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,6 +18,18 @@ EOF
     git config --global user.name "GitHub Actions"
 }
 
+is_defined() {
+    [ ! -z "${1}" ]
+}
+
+get_value() {
+    if is_defined "${1}"; then
+        return "${1}"
+    fi
+
+    return "${2}"
+}
+
 
 # This section only runs if there have been file changes
 echo "Checking for uncommitted changes in the git working tree."
@@ -30,15 +42,10 @@ then
     # Switch to branch from current Workflow run
     git checkout $INPUT_BRANCH
 
-    if [ -z ${INPUT_FILE_PATTERN+x} ];
-    then
-        git add .
-    else
-        echo "INPUT_FILE_PATTERN value: $INPUT_FILE_PATTERN";
-        git add $INPUT_FILE_PATTERN
-    fi
+    git add get_value "${INPUT_FILE_PATTERN}" '.'
 
-    git commit -m "$INPUT_COMMIT_MESSAGE" --author="$GITHUB_ACTOR <$GITHUB_ACTOR@users.noreply.github.com>" $INPUT_COMMIT_OPTIONS
+    git commit -m "$INPUT_COMMIT_MESSAGE" --author="$GITHUB_ACTOR <$GITHUB_ACTOR@users.noreply.github.com>" get_value "${INPUT_COMMIT_OPTIONS}" ''
+
 
     git push --set-upstream origin "HEAD:$INPUT_BRANCH"
 else
