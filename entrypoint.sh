1ad5315ba755cc61fad7498275da51798656c425
Gömöri Zsolt <gomori.zsolt1995@gmail.com>
2019-11-01 09:51:19 +0100

    Fix log statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,7 +42,7 @@ then
     fi
 
     if is_defined "${INPUT_COMMIT_OPTIONS}"; then
-        echo "INPUT_FILE_PATTERN: ${INPUT_COMMIT_OPTIONS}"
+        echo "INPUT_COMMIT_OPTIONS: ${INPUT_COMMIT_OPTIONS}"
 
         git commit -m "$INPUT_COMMIT_MESSAGE" --author="$GITHUB_ACTOR <$GITHUB_ACTOR@users.noreply.github.com>" "${INPUT_COMMIT_OPTIONS}"
     else
