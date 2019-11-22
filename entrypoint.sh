65d8bdf167b3f43d9687f86e2666984efea176d2
Andreas Möller <am@localheinz.com>
2019-11-22 11:52:51 +0100

    Fix: Add files that are not tracked yet

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,26 +19,30 @@ EOF
     git config --global user.name "GitHub Actions"
 }
 
-# This section only runs if there have been file changes
+git_setup
+
+echo "INPUT_BRANCH value: $INPUT_BRANCH";
+
+# Switch to branch from current Workflow run
+git checkout $INPUT_BRANCH
+
+echo "INPUT_FILE_PATTERN: ${INPUT_FILE_PATTERN}"
+
+git add "${INPUT_FILE_PATTERN}"
+
 echo "Checking for uncommitted changes in the git working tree."
-if ! git diff --quiet
+
+if ! git diff --staged --quiet
 then
-    git_setup
-
-    echo "INPUT_BRANCH value: $INPUT_BRANCH";
-
-    # Switch to branch from current Workflow run
-    git checkout $INPUT_BRANCH
-
-    echo "INPUT_FILE_PATTERN: ${INPUT_FILE_PATTERN}"
-
-    git add "${INPUT_FILE_PATTERN}"
-
-    echo "INPUT_COMMIT_OPTIONS: ${INPUT_COMMIT_OPTIONS}"
-
-    git commit -m "$INPUT_COMMIT_MESSAGE" --author="$GITHUB_ACTOR <$GITHUB_ACTOR@users.noreply.github.com>" ${INPUT_COMMIT_OPTIONS:+"$INPUT_COMMIT_OPTIONS"}
-
-    git push --set-upstream origin "HEAD:$INPUT_BRANCH"
-else
     echo "Working tree clean. Nothing to commit."
+
+    exit 0
 fi
+
+echo "Found added/changed files."
+
+echo "INPUT_COMMIT_OPTIONS: ${INPUT_COMMIT_OPTIONS}"
+
+git commit -m "$INPUT_COMMIT_MESSAGE" --author="$GITHUB_ACTOR <$GITHUB_ACTOR@users.noreply.github.com>" ${INPUT_COMMIT_OPTIONS:+"$INPUT_COMMIT_OPTIONS"}
+
+git push --set-upstream origin "HEAD:$INPUT_BRANCH"
