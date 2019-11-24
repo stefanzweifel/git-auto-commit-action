fb3de054517a45628867e73a4cf10f7d6930aba0
Stefan Zweifel <hello@stefanzweifel.io>
2019-11-24 12:21:42 +0100

    Revert "Fix: Add files that are not tracked yet"
    This reverts commit 65d8bdf167b3f43d9687f86e2666984efea176d2.

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,30 +19,26 @@ EOF
     git config --global user.name "GitHub Actions"
 }
 
-git_setup
-
-echo "INPUT_BRANCH value: $INPUT_BRANCH";
-
-# Switch to branch from current Workflow run
-git checkout $INPUT_BRANCH
-
-echo "INPUT_FILE_PATTERN: ${INPUT_FILE_PATTERN}"
-
-git add "${INPUT_FILE_PATTERN}"
-
+# This section only runs if there have been file changes
 echo "Checking for uncommitted changes in the git working tree."
-
-if ! git diff --staged --quiet
+if ! git diff --quiet
 then
+    git_setup
+
+    echo "INPUT_BRANCH value: $INPUT_BRANCH";
+
+    # Switch to branch from current Workflow run
+    git checkout $INPUT_BRANCH
+
+    echo "INPUT_FILE_PATTERN: ${INPUT_FILE_PATTERN}"
+
+    git add "${INPUT_FILE_PATTERN}"
+
+    echo "INPUT_COMMIT_OPTIONS: ${INPUT_COMMIT_OPTIONS}"
+
+    git commit -m "$INPUT_COMMIT_MESSAGE" --author="$GITHUB_ACTOR <$GITHUB_ACTOR@users.noreply.github.com>" ${INPUT_COMMIT_OPTIONS:+"$INPUT_COMMIT_OPTIONS"}
+
+    git push --set-upstream origin "HEAD:$INPUT_BRANCH"
+else
     echo "Working tree clean. Nothing to commit."
-
-    exit 0
 fi
-
-echo "Found added/changed files."
-
-echo "INPUT_COMMIT_OPTIONS: ${INPUT_COMMIT_OPTIONS}"
-
-git commit -m "$INPUT_COMMIT_MESSAGE" --author="$GITHUB_ACTOR <$GITHUB_ACTOR@users.noreply.github.com>" ${INPUT_COMMIT_OPTIONS:+"$INPUT_COMMIT_OPTIONS"}
-
-git push --set-upstream origin "HEAD:$INPUT_BRANCH"
