eda98fb9e50d5c495c5361ac43ea95c52f393459
Stefan Zweifel <hello@stefanzweifel.io>
2020-02-11 21:04:18 +0100

    Simplify index.js

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
+const spawn = require('child_process').spawn;
+const path = require("path");
 
-async function run() {
-  try {
-    await exec.exec('sh', ['entrypoint.sh']);
-  }
-  catch (error) {
-    core.setFailed(error.message);
-  }
-}
+const exec = (cmd, args=[]) => new Promise((resolve, reject) => {
+    console.log(`Started: ${cmd} ${args.join(" ")}`)
+    const app = spawn(cmd, args, { stdio: 'inherit' });
+    app.on('close', code => {
+        if(code !== 0){
+            err = new Error(`Invalid status code: ${code}`);
+            err.code = code;
+            return reject(err);
+        };
+        return resolve(code);
+    });
+    app.on('error', reject);
+});
 
-run()
+const main = async () => {
+    await exec('sh', [path.join(__dirname, './entrypoint.sh')]);
+};
+
+main().catch(err => {
+    console.error(err);
+    console.error(err.stack);
+    process.exit(err.code || -1);
+})
