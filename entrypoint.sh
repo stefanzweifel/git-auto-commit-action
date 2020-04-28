1ee136f4392f6e7b95bf621c05b5e10601f022de
Stefan Zweifel <hello@stefanzweifel.io>
2020-04-28 13:12:43 +0200

    Inline user config with tag command

--- entrypoint.sh
+++ entrypoint.sh
@@ -66,7 +66,7 @@ _tag_commit() {
     if [ -n "$INPUT_TAGGING_MESSAGE" ]
     then
         echo "::debug::Create tag $INPUT_TAGGING_MESSAGE";
-        git tag -a "$INPUT_TAGGING_MESSAGE" -m "$INPUT_TAGGING_MESSAGE";
+        git -c user.name="$INPUT_COMMIT_USER_NAME" -c user.email="$INPUT_COMMIT_USER_EMAIL" tag -a "$INPUT_TAGGING_MESSAGE" -m "$INPUT_TAGGING_MESSAGE";
     else
         echo " No tagging message supplied. No tag will be added.";
     fi
