49e28abb3fdf4552230e4079ca389973c3c6a004
Stefan Zweifel <hello@stefanzweifel.io>
2020-06-25 20:31:48 +0200

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -eu
 _main() {
     _switch_to_repository
 
-    if _git_is_dirty || [ -n "$INPUT_SKIP_DIRTY_CHECK" ]; then
+    if _git_is_dirty -o [ "$INPUT_SKIP_DIRTY_CHECK" = true ]; then
 
         echo "::set-output name=changes_detected::true";
 
