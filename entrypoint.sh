6456430b6b0b92ff51a83476962d42f640b4429b
Stefan Zweifel <hello@stefanzweifel.io>
2020-10-15 20:15:31 +0200

    Ignore shellchecks

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,6 +48,8 @@ _switch_to_branch() {
 
 _add_files() {
     echo "INPUT_FILE_PATTERN: ${INPUT_FILE_PATTERN}";
+
+    # shellcheck disable=SC2086
     git add ${INPUT_FILE_PATTERN};
 }
 
@@ -55,6 +57,7 @@ _local_commit() {
     echo "INPUT_COMMIT_OPTIONS: ${INPUT_COMMIT_OPTIONS}";
     echo "::debug::Apply commit options ${INPUT_COMMIT_OPTIONS}";
 
+    # shellcheck disable=SC2206
     INPUT_COMMIT_OPTIONS_ARRAY=( $INPUT_COMMIT_OPTIONS );
 
     git -c user.name="$INPUT_COMMIT_USER_NAME" -c user.email="$INPUT_COMMIT_USER_EMAIL" \
@@ -80,6 +83,7 @@ _push_to_github() {
     echo "INPUT_PUSH_OPTIONS: ${INPUT_PUSH_OPTIONS}";
     echo "::debug::Apply push options ${INPUT_PUSH_OPTIONS}";
 
+    # shellcheck disable=SC2206
     INPUT_PUSH_OPTIONS_ARRAY=( $INPUT_PUSH_OPTIONS );
 
     if [ -z "$INPUT_BRANCH" ]
