1e826be61c9226fa82503b1c17138d92f5feda1f
Stefan Zweifel <stefanzweifel@users.noreply.github.com>
2020-10-24 11:28:52 +0200

    Revert "Fix checking out branch names with / chars"

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ _switch_to_branch() {
     git fetch;
 
     # Switch to branch from current Workflow run
-    git checkout "$INPUT_BRANCH" --;
+    git checkout $INPUT_BRANCH;
 }
 
 _add_files() {
