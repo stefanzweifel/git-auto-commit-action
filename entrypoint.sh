aa66e830693a9d80415c25c5fe64388a4bb5b8e0
Stefan Zweifel <hello@stefanzweifel.io>
2022-09-14 20:18:23 +0200

    Fix Shellcheck Issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,7 @@ _git_is_dirty() {
     echo "::debug::Apply status options ${INPUT_STATUS_OPTIONS}";
 
     echo "INPUT_FILE_PATTERN: ${INPUT_FILE_PATTERN}";
-    read -a INPUT_FILE_PATTERN_EXPANDED <<< "$INPUT_FILE_PATTERN";
+    read -r -a INPUT_FILE_PATTERN_EXPANDED <<< "$INPUT_FILE_PATTERN";
 
     # shellcheck disable=SC2086
     [ -n "$(git status -s $INPUT_STATUS_OPTIONS -- ${INPUT_FILE_PATTERN_EXPANDED:+${INPUT_FILE_PATTERN_EXPANDED[@]}})" ]
@@ -78,7 +78,7 @@ _add_files() {
     echo "::debug::Apply add options ${INPUT_ADD_OPTIONS}";
 
     echo "INPUT_FILE_PATTERN: ${INPUT_FILE_PATTERN}";
-    read -a INPUT_FILE_PATTERN_EXPANDED <<< "$INPUT_FILE_PATTERN";
+    read -r -a INPUT_FILE_PATTERN_EXPANDED <<< "$INPUT_FILE_PATTERN";
 
     # shellcheck disable=SC2086
     git add ${INPUT_ADD_OPTIONS} ${INPUT_FILE_PATTERN:+"${INPUT_FILE_PATTERN_EXPANDED[@]}"};
