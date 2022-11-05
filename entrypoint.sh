c2499a189e79015f38cf17ff034a81bee75fbe4d
Stefan Zweifel <hello@stefanzweifel.io>
2022-11-05 11:34:02 +0100

    Set proper error message

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,6 +49,7 @@ _check_if_git_is_available() {
         echo "git found";
         echo "::debug::git binary found";
     else
+        echo "::error ::git not found. git-auto-commit requires git to be available."
         echo "git not found. git-auto-commit requires git to be available." >&2
         exit 1
     fi
